2885676b14a7bd9f5399884af90f1f95cbd0d22b
“liuxue” <lixue@gozap.com>
2021-10-25 18:32:38 +0800

    “build was configured to prefer settings repositories over project repositories”解决方法

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-
     repositories {
         maven { setUrl("https://maven.aliyun.com/nexus/content/groups/public/") }
         maven { setUrl("https://maven.aliyun.com/nexus/content/repositories/jcenter") }
-        maven { setUrl("https://jitpack.io") }
 
         google()
         mavenCentral()
@@ -12,21 +9,21 @@ buildscript {
     dependencies {
         classpath ("com.android.tools.build:gradle:7.0.3")
         classpath(kotlin(module = "gradle-plugin", version = Configuration.Versions.kotlin_version))
-
+        classpath("com.github.dcendents:android-maven-gradle-plugin:1.5")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-
 allprojects {
     repositories {
         maven { setUrl("https://maven.aliyun.com/nexus/content/groups/public/") }
         maven { setUrl("https://maven.aliyun.com/nexus/content/repositories/jcenter") }
-        maven { setUrl("https://jitpack.io") }
-
         google()
         mavenCentral()
+        flatDir {
+            dirs("libs")
+        }
     }
 }
 
